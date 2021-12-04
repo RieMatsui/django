30260b134a5d18c9e4f20e9a46adb32b25a1f546
RieMatsui <funkybass0405@gmail.com>
2021-12-04 23:41:23 +0900

    画面を表示

--- app/andOr/urls.py
+++ app/andOr/urls.py
@@ -2,13 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'andOr'
 urlpatterns = [
-    # ex: /andOr/
-    path('', views.index, name='index'),
-    # ex: /andOr/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /andOr/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /andOr/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
