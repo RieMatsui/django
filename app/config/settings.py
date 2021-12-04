f797ce9e615fe58f8dbdb0c88a0654e3e7cb7e86
RieMatsui <funkybass0405@gmail.com>
2021-12-04 19:26:40 +0900

    管理画面の修正。モデルの修正。

--- app/config/settings.py
+++ app/config/settings.py
@@ -44,6 +44,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'andOr.apps.AndorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
