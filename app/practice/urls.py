b15c1aef63aba341d2cf647073a448d5b6f6455d
RieMatsui <funkybass0405@gmail.com>
2022-10-10 04:31:31 +0900

    リストの変更まで

--- app/practice/urls.py
+++ app/practice/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+    path("", views.Index.as_view(), name="index"),
+    path("list", views.List.as_view(), name="list"),
+    ]
