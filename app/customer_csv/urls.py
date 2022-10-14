a4bf1a1ed7fb2bdfa78a619efab33785552cae8e
RieMatsui <funkybass0405@gmail.com>
2022-10-15 08:24:22 +0900

    顧客台帳と売上データを集計するサンプル

--- app/customer_csv/urls.py
+++ app/customer_csv/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import sale_data
+from .views import customer
 from django.urls import include, path
 
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
                   path('item_master', sale_data.item_master, name='item_master'),
                   path('transaction', sale_data.transaction, name='transaction'),
                   path('transaction_detail', sale_data.transaction_detail, name='transaction_detail'),
-                  path('sale_data', sale_data.sale_data, name='sale_data')
+                  path('sale_data', sale_data.sale_data, name='sale_data'),
+                  path('customer', customer.Customer.as_view(), name='index'),
               ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
---
+++ app/customer_csv/views/customer.py
@@ -0,0 +1,29 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from pandas import pandas as pd
+import os
+
+
+class Customer(TemplateView):
+
+    def get(self, request):
+        # テンプレートファイル連携
+        template_name = 'customer_csv/customer/index.html'
+        filePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/file/customer/sale.csv'
+        sale = pd.read_csv(filePath)
+        sale_num = len(sale)
+
+        filePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/file/customer/customer.xlsx'
+        customer = pd.read_excel(filePath)
+        customer_num = len(customer)
+
+        context = {
+            'sale': sale,
+            'sale_num': sale_num,
+            'customer': customer,
+            'customer_num': customer_num,
+        }
+        return render(request, template_name, context)
+
+
+customerView = Customer.as_view()
