f3494d7ec98ae0234a45ddfcc5bf868d8a7aff38
RieMatsui <funkybass0405@gmail.com>
2022-10-15 21:20:51 +0900

    リファクタリング

--- app/processing_data/urls.py
+++ app/processing_data/urls.py
@@ -1,15 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import sale_data
+from .views import sale
 from .views import customer
 from django.urls import include, path
 
 urlpatterns = [
-                  path('', sale_data.index, name='index'),
-                  path('item_master', sale_data.item_master, name='item_master'),
-                  path('transaction', sale_data.transaction, name='transaction'),
-                  path('transaction_detail', sale_data.transaction_detail, name='transaction_detail'),
-                  path('sale_data', sale_data.sale_data, name='sale_data'),
+                  path('sale', sale.index, name='index'),
+                  path('sale/item_master', sale.item_master, name='item_master'),
+                  path('sale/transaction', sale.transaction, name='transaction'),
+                  path('sale/transaction_detail', sale.transaction_detail, name='transaction_detail'),
+                  path('sale/sale_data', sale.sale_data, name='sale_data'),
                   path('customer', customer.index, name='index'),
                   path('customer/sale_per_product', customer.get_sale_per_product, name='sale_per_product'),
               ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
