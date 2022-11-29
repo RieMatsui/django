d643be6ea85b4e4c89dbcbd5191d45dc68b1bc65
RieMatsui <funkybass0405@gmail.com>
2022-11-29 23:33:38 +0900

    GSAPの導入

---
+++ client/mysite/src/components/artwork/CircleAnimation.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card class="mt-5 p-4">
+        <div>
+            <h1 class="m-3">GASP Sample</h1>
+            <div class="circle"></div>
+        </div>
+    </v-card>
+</template>
+
+<script>
+    import {gsap, Elastic} from "gsap";
+
+    export default {
+        name: 'cercleMove',
+        mounted() {
+            const animation = gsap.to(".circle", {
+                backgroundColor: "blue",
+                x: 500,
+                ease: Elastic.easeOut,
+                duration: 5,
+            })
+            animation;
+        }
+
+    }
+</script>
+
+<style>
+    .circle {
+        background-color: aqua;
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        text-align: left;
+    }
+</style>
